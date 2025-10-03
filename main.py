7c378953d67d9611bf1f90dfd8b8e3ef1f81fbe4
Foy <s225002731@gmail.com>
2025-10-03 15:22:30 +0800

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+# 這是一個範例 Python 腳本。
+
+# 按 Shift+F10 執行或將其取代為您的程式碼。
+# 按 雙擊 Shift 在所有地方搜尋類別、檔案、工具視窗、動作和設定。
+
+
+def print_hi(name):
+    # 在下面的程式碼行中使用中斷點來偵錯腳本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切換中斷點。
+
+
+# 按裝訂區域中的綠色按鈕以執行腳本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 存取 https://www.jetbrains.com/help/pycharm/ 獲取 PyCharm 幫助
